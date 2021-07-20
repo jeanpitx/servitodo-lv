03805d7f71af3a274c5a4c4c475c94d2062372ed
jeanpitx <yojean02@hotmail.com>
2021-07-20 14:34:55 -0500

    complete auth template in all views and add view test and functionality in Verify Email.

---
+++ resources/js/Pages/Test.vue
@@ -0,0 +1,32 @@
+<template>
+    <admin-layout>
+        <template #header>
+            Dashboard
+        </template>
+
+        <div class="py-8">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        This is a test secure area!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </admin-layout>
+</template>
+
+<script>
+    import AdminLayout from '@/Layouts/Admin'
+
+    export default {
+        components: {
+            AdminLayout,
+        },
+
+        props: {
+            auth: Object,
+            errors: Object,
+        },
+    }
+</script>
